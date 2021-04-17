2bbb76d2d6c915b1dda2e5b5a5a750c29da5debd
MLHaddon <haddon.mike2@outlook.com>
2021-04-17 15:19:48 -0500

    Initial Commit

---
+++ Algorithms/ReverseString.js
@@ -0,0 +1,11 @@
+// 1.
+
+/**  String Reversal  **/
+
+// Given a string of characters as input, write a function that returns it with the characters reversed.
+
+function reverseString(str) {
+  let newString = str.split('');
+  console.log(newString);
+};
+
